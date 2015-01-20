1457c19b3f4e7bd36799c87efe743c6d3e78ee07
hsync <felixschulze@hotmail.de>
2015-01-20 20:38:19 +0100

    write out scan function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 #Felix Schulze
 #2015/01/20
+
 
 
 def searchCode(ScanCode):
@@ -9,28 +10,41 @@ def searchCode(ScanCode):
     # send Ean-Code to Novolume OffShop Server
     # and get item description and price
     # if item could not be found send item = 0 !
+    item = "Coke 0.5l"
+    # return price as string !
+    price = "1.00"
 
-    #return item, price
+    return item, price
+
+
 
 # Start of main program
-print ("\n\rOffShopScanner Software\n\rVer.: 0.9 --- [NoVolume] Felix Schulze 2015")
+print ("\n\rOffShopScanner Software\n\rVer.: 0.9 --- NoVolume\n\rFelix Schulze 2015")
 
 # Wait for login via rfid card
 
 # as long as the purchase is not completed scan for ean-codes
 while 1:
-  ScanCode= input("\nScan... : ")
 
-  item, price = searchCode(ScanCode)
+    # Scan ean-codes
+    ScanCode= raw_input("\nScan... : ")
 
-  if (item == "0"):
-    print ("Item not found")
-    i-=1
-    continue
+    # Check if END is typed and leave while loop
+    if(ScanCode == "END"):
+        print ("Purchased finisched")
+        break
 
+    # send request to server and get descripton and price back
+    item, price = searchCode(ScanCode)
 
-  print ("Item    : " + item)
-  print ("Price   : " + price + " EUR")
-  i+=1
+    # print warning if scanned item could not be found
+    if (item == "0"):
+        print ("Item not found")
+        continue
+
+    # print item informations on the screen
+    print ("Item    : " + item)
+    print ("Price   : " + price + " EUR")
+    # send item information via UART to microcontroller
 
 # if purchase is finished send bill to Novolume Shop Server
