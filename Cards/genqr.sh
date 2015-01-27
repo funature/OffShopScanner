d74874a413b9a37d81495b14f30ebfdb8c8d3b30
hsync <felixschulze@hotmail.de>
2015-01-27 23:02:16 +0100

    optimize genqr skript

--- Cards/genqr.sh
+++ Cards/genqr.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/bash
 #You have to install "pillow" and "qrcode" for python2.7
 #before running this skritp
-#This script creats 100 QR-Codes and delete the unnecessaries.
-#Because I dont know how to write an abort condition.
 
 
 for((i=2; i <102; i++))
 do
 	nameId=`head -n $i ./member.csv | tail -n 1 | tr -d "\n" | tr -d "\r" | sed "s/,/-/g"`
 
+	#cp for String compare
+	S1=`echo $nameId | awk '{print substr($0,11)}'`
+	S2="--"
+
+	if [ "$S1" = "$S2" ];
+	then
+		echo "::Finished"
+		break
+	fi	
+	
 	echo $nameId
 
 	qr --factory=svg-fragment $nameId > $nameId.svg
 done
 
-rm ./*--.svg
